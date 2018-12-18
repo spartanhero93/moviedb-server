314e48104a09aa0f5309adaadd1eceed4da05d5f
spartanhero93 <spartanhero93@gmail.com>
2018-12-18 12:13:05 -0500

    added search route and function

--- API/index.js
+++ API/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const callMovieAPI = require('../service/movidbapi')
 const callTVShowsAPI = require('../service/tvShowsAPI')
+const querySearch = require('../service/querySearch')
 
 /** Search the MovieDB api based on client request type */
 router.get('/movies/:name/:pageNum', async (req, res) => {
@@ -15,6 +16,14 @@ router.get('/movies/:name/:pageNum', async (req, res) => {
 router.get('/tv/:name/:pageNum', async (req, res) => {
   try {
     const data = await callTVShowsAPI(req.params.name, req.params.pageNum)
+    await res.send({ data })
+  } catch (error) {
+    console.log(error)
+  }
+})
+router.get('/search/:query/:pageNum', async (req, res) => {
+  try {
+    const data = await querySearch(req.params.query, req.params.pageNum)
     await res.send({ data })
   } catch (error) {
     console.log(error)
---
+++ service/querySearch.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+
+const querySearch = async (params, pageNum) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/search/multi?api_key=${process.env.API_KEY}
+      &language=en-US&query=${params}&page=${pageNum}&include_adult=false`
+    )
+    const { data } = await response
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = querySearch
